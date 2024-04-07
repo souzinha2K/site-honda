d94a262c94f589c8438b99119c45e519f2b830c1
souzinha2K <160570062+souzinha2K@users.noreply.github.com>
2024-04-07 13:27:35 -0300

    Site Honda

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMode () {
+ const html = document.documentElement
+ html.classList.toggle("light")
+
+ const img = document.querySelector ("#profile img")
+
+ if (html.classList.contains("light")) {
+
+ img.setAttribute("src", "./assets/yamaha.png")
+ } else {
+
+  img.setAttribute("src", "./assets/honda.png")
+ }
+
+}
